abc3dd6861c416514482a700f545a15a3f9ad66f
Eric Eller <ericaeeller34@gmail.com.br>
2025-04-26 10:06:33 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("27999652362", "Iphone 16 PRO", "176y39990dj", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("        ");
+
+
+Nokia nokia = new Nokia("27997392804", "Nokia Tijolão", "awe3213d21", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
